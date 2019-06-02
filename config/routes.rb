b123ad683e3c9de44df4b657ab5fb8aa29f14d5a
OneBoSS2018 <bob204981@gmail.com>
2019-06-02 19:41:56 +0300

    Add controller/model question

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'questions/new'
+  get 'questions/index'
+  get 'questions/show'
+  get 'questions/edit'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
