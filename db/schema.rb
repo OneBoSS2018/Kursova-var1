a143d462e4bce51a86a09c0b787579a0394782af
OneBoSS2018 <bob204981@gmail.com>
2019-06-02 19:50:24 +0300

    Add controller/model answer && update routes && add associations question with answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_164058) do
+ActiveRecord::Schema.define(version: 2019_06_02_164545) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 2019_06_02_164058) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "answers", force: :cascade do |t|
+    t.string "text"
+    t.integer "question_id"
+    t.boolean "right"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
